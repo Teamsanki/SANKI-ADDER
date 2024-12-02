ead0b9866d170e830bd1b9ff0970eced9ff64191
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-12-02 22:18:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 
 # ----------------ʜᴇʀᴇ-ᴄᴏɴғɪɢ------------------#
 
-API_ID = environ['24740695']
-API_HASH = environ['a95990848f2b93b8131a4a7491d97092']
-BOT_TOKEN = environ['7821314898:AAHgRpDfbpfsqefrGUUYKlOtigbqNSc4gA0']
+API_ID = environ["24740695"]
+API_HASH = environ["a95990848f2b93b8131a4a7491d97092"]
+BOT_TOKEN = environ["7821314898:AAHgRpDfbpfsqefrGUUYKlOtigbqNSc4gA0"]
 OWNER_IDS = list(map(int, getenv("OWNER_IDS", "7877197608").split()))
 PREMIUM_IDS = list(map(int, getenv("PREMIUM_IDS", "7877197608").split()))
 
